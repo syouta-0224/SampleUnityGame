0d2b6b6403f09a0e0bccb689673151ff568037c9
lucreziaheart <LucreziaHeart@outlook.jp>
2021-03-27 18:15:22 +0900

    first commit

---
+++ Assets/Scripts/Hello.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hello : MonoBehaviour
+{
+    private string hello = "Hello!!!";
+    private void Start()
+    {
+        Debug.Log(hello);
+    }
+}
