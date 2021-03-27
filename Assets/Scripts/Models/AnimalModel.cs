8a83b04a0b662cd5f4b99b8eed84de746f916969
lucreziaheart <LucreziaHeart@outlook.jp>
2021-03-27 20:41:42 +0900

    #1 add animal func wip

---
+++ Assets/Scripts/Models/AnimalModel.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Scripts.Models
+{
+    public class AnimalModel : MonoBehaviour
+    {
+        public int Id;
+        public string Name;
+    }
+}
+
